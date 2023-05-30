4fa75b159a4c69f9b1b3f346959ee93924bf8481
Dave Voutila <voutilad@gmail.com>
2023-05-30 18:18:50 -0400

    add example tests

---
+++ lib/src/test/java/com/redpanda/connect/ExampleSinkTaskTest.java
@@ -0,0 +1,14 @@
+package com.redpanda.connect;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+public class ExampleSinkTaskTest {
+  @Test
+  public void simpleExampleSinkTaskTest() {
+    final var task = new ExampleSinkTask();
+    Assertions.assertDoesNotThrow(() -> task.put(Collections.EMPTY_LIST), "Should not puke.");
+  }
+}
---
+++ lib/src/test/java/com/redpanda/connect/ExampleSourceTaskTest.java
@@ -0,0 +1,20 @@
+package com.redpanda.connect;
+
+import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ExampleSourceTaskTest {
+  @Test
+  public void testGeneratesDummyDataTellingTheTime() throws InterruptedException {
+    final var task = new ExampleSourceTask();
+
+    // Probably wouldn't test a real poll() call like this, but just an example.
+    final List<SourceRecord> data = task.poll();
+    Assertions.assertEquals(2, data.size(), "should have 2 dummy records");
+    final var head = data.get(0).value().toString();
+    Assertions.assertTrue(head.startsWith("The time is now "));
+  }
+}
