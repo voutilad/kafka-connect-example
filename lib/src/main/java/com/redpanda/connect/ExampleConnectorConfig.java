5d5f3867735265426d2f5f5c0998d3504c789e7b
Dave Voutila <voutilad@gmail.com>
2023-05-30 17:19:24 -0400

    add a fake number option and change topic to HIGH

--- lib/src/main/java/com/redpanda/connect/ExampleConnectorConfig.java
+++ lib/src/main/java/com/redpanda/connect/ExampleConnectorConfig.java
@@ -11,10 +11,13 @@ public class ExampleConnectorConfig extends AbstractConfig {
   public static final String KEY_USERNAME = "example.username";
 
   public final String password;
-  public static final String KEY_PASSWORD = "example.username";
+  public static final String KEY_PASSWORD = "example.password";
 
   public final String topic;
-  public static final String KEY_TOPIC = "example.username";
+  public static final String KEY_TOPIC = "example.topic";
+
+  public final int number;
+  public static final String KEY_NUMBER = "example.number";
 
 
   public ExampleConnectorConfig(Map<String, String> props) {
@@ -23,12 +26,14 @@ public class ExampleConnectorConfig extends AbstractConfig {
     username = this.getString(KEY_USERNAME);
     password = this.getString(KEY_PASSWORD);
     topic = this.getString(KEY_TOPIC);
+    number = this.getInt(KEY_NUMBER);
   }
 
   public static ConfigDef config() {
     return new ConfigDef()
         .define(KEY_USERNAME, ConfigDef.Type.STRING, "D.Fault User", ConfigDef.Importance.HIGH, "A username")
         .define(KEY_PASSWORD, ConfigDef.Type.STRING, "********", ConfigDef.Importance.HIGH, "A password")
-        .define(KEY_TOPIC, ConfigDef.Type.STRING, "example-topic", ConfigDef.Importance.MEDIUM, "Target or source topic");
+        .define(KEY_TOPIC, ConfigDef.Type.STRING, "example-topic", ConfigDef.Importance.HIGH, "Target or source topic")
+        .define(KEY_NUMBER, ConfigDef.Type.INT, 1983, ConfigDef.Importance.MEDIUM, "Just a number to show another config option.");
   }
 }
